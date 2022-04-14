6252560fe54b950496a30a4eda6ff0b6869a266a
CJMaxwell <chukwunonyeremmaxwell@gmail.com>
2022-04-14 08:09:13 +0100

    rebasing maintenance

---
+++ src/app/components/dashboard/assets/maintenance/maintenance-detail/maintenance-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fms-maintenance-detail',
+  templateUrl: './maintenance-detail.component.html',
+  styleUrls: ['./maintenance-detail.component.scss']
+})
+export class MaintenanceDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
